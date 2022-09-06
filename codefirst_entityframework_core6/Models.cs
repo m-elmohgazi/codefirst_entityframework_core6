6c4adc2e992c7dbbb8934a2809257f4ca90dc87f
Mohammed Elmoghazi <mohammed.elmoghazi@nagwa.com>
2022-09-06 08:34:29 +0200

    Add project files.

---
+++ codefirst_entityframework_core6/Models.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codefirst_entityframework_core6
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+    }
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediate,
+        Advanced
+    }
+
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext(DbContextOptions<PlutoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+    }
+}
---
+++ codefirst_entityframework_core6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start");
+
+/*
+ * create your model classes
+ * install entity framework
+ * create dbcontext
+ * create the dbset in DbContext classs
+ * 
+ */
